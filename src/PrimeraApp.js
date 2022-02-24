c4fab3e6cb28cf6ae05ae9e276f1a46ab06ca978
Guillermo Izalde <izalde.guillermo45@gmail.com>
2022-02-24 08:47:29 -0600

    Fragment

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -4,3 +4,15 @@ import PropTypes from 'prop-types';
 //import React, { Fragment } from 'react'; {
 
 
+const PrimeraApp = {} => {
+
+return {
+    <>
+    
+    <h1>Hola Mundo</h1>
+    <p>Mi primera aplicación</p>
+    </>
+
+    };
+}
+
