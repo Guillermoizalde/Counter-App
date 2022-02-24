5b8b2afeb4cafef9130c1ad72d69bb01e238eb3e
Guillermo Izalde <izalde.guillermo45@gmail.com>
2022-02-24 08:15:27 -0600

    Hola Mundo

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+//import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
+
+import './index.css';
+
+
+
+
+const divRoot = document.querySelector('#app');
+
+
+ReactDom.render( <CounterApp value = {10}/> , divRoot );
+
