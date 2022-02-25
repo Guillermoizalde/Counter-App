12934b2daff8852f0d51f2c21abded70c88bfcd3
Guillermo Izalde <izalde.guillermo45@gmail.com>
2022-02-24 19:59:58 -0600

    HandleSubstrac HandleReset

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -9,12 +9,33 @@ const CounterApp = ([value]) => {
     const handleAdd = {e} => {
         setCounter (counter + 1 );
 
+        
+    //handleAdd
+    const handleAdd = () => {
+        setCounter (counter + 1 );
+    }
+
+    //Quitar
+    const putOff = () => {
+        setCounter (counter - 1);
+    }
+
+    //Resetear
+    const reset = () => {
+        setCounter (counter - counter);
+
+
     }
 
   return (
     <div>
         <h1>CounterApp</h1>
         <h2> { counter } </h2>
+
+        <button onClick =  { handleAdd } >+1</button>
+        <button onClick = {reset}>Reset</button>
+        <button onClick = {putOff}>-1</button>
+
 
         <button onClick={ {handleAdd} >+1</Button>
     </div>
