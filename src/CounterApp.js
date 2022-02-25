11242d9f6b668873d3088d52a4f5762b333b1b4c
Guillermo Izalde <izalde.guillermo45@gmail.com>
2022-02-24 19:50:37 -0600

    UseState hook

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,10 +2,21 @@ import React, { useDebugValue } from 'react'
 import PropTypes from 'prop-types'
 
 const CounterApp = ([value]) => {
+
+    const [counter, setCounter] = useState(0);
+
+    //handleAdd
+    const handleAdd = {e} => {
+        setCounter (counter + 1 );
+
+    }
+
   return (
     <div>
         <h1>CounterApp</h1>
-        <h2> { value } </h2>
+        <h2> { counter } </h2>
+
+        <button onClick={ {handleAdd} >+1</Button>
     </div>
   )
 }
